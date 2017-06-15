fbdbc031f13281c1eb339a0b63d78ec90a3973ea
jbogaardt <jbogaardt@gmail.com>
2017-06-15 10:13:23 -0700

    Added basic chainladder functionality
    Added basic chainladder functionality

--- python/R Data Converter.py
+++ python/R Data Converter.py
@@ -20,10 +20,25 @@ def create_python_df(name):
         df = pd.DataFrame(r(name), index = r('rownames(' + name + ')').astype(float), columns=r('colnames(' + name + ')'))
         df = df.rename_axis("dev", axis="columns")
         df.index.name = 'origin'
+    elif r('class('+ name + ')')[0] == 'list':
+        df_dict = {}
+        for i in range(r('length('+ name + ')')[0]):
+            subname = r('names('+ name + '[' + str(i + 1) + '])')[0] # Name of subtriangle
+            df = pd.DataFrame(r(name + '$' + subname))
+            df.columns = df.columns.values.astype(str)
+            df = df.rename_axis("dev", axis="columns")
+            df = df.rename_axis("origin", axis="rows")
+            df = df.replace(-2147483648, np.nan)
+            df_dict[name + '$' + subname] = df
+        return df_dict
     # Integer type NAs return as -2147483648 - No clue why, but these are NA is the R environment
     df = df.replace(-2147483648, np.nan)
     return df
 
 for item in datasets:
-    df = create_python_df(item)
-    df.to_pickle(path + item)    
+    return_val = create_python_df(item)
+    if type(return_val) == type(dict()):
+        for k, v in return_val.items():
+            v.to_pickle(path + k) 
+    if type(return_val) == type(pd.DataFrame()):
+        return_val.to_pickle(path + item)    
--- python/Triangles.py
+++ python/Triangles.py
@@ -2,10 +2,12 @@
 
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
  
 class triangle:
-    def __init__(self, data=None, origin = None, dev = None, values = 'values', dataform = 'triangle'):
+    def __init__(self, data=None, origin = None, dev = None, values = None, dataform = 'triangle'):
         # Currently only support pandas dataframes as data
         if str(type(data)) != '<class \'pandas.core.frame.DataFrame\'>':
             print('Data is not a proper triangle')
@@ -14,14 +16,16 @@ class triangle:
         self.data = data
         self.origin = origin
         if origin == None:
-            origin_in_col_bool, origin_in_index_bool = self.__set_origin(origin)   
+            origin_in_col_bool, origin_in_index_bool = self.__set_origin()   
         self.dev = dev     
         if dev == None:
-            dev_in_col_bool = self.__set_dev(dev) 
+            dev_in_col_bool = self.__set_dev() 
         self.dataform = dataform
         if dev_in_col_bool == True and origin_in_col_bool == True:
             self.dataform = 'tabular'
         self.values = values
+        if values == None:
+            self.__set_values() 
           
     def dataAsTable(self, inplace=False):
         # will need to create triangle class that has origin and dev
@@ -36,7 +40,9 @@ class triangle:
                 self.data= lx[['dev','values']]
                 self.dataform = 'tabular'
                 self.dev = 'dev'
-        return lx[['dev','values']]
+                return lx[['dev','values']]
+        else:
+            return
         
 
     def dataAsTriangle(self, inplace=False):
@@ -68,14 +74,13 @@ class triangle:
             self.data = incr        
         return incr   
     
-    def __set_origin(self, origin):
+    def __set_origin(self):
         ##### Identify Origin Profile ####
         origin_names = ['accyr', 'accyear', 'accident year', 'origin', 'accmo', 'accpd', 
                         'accident month']
         origin_in_col_bool = False
         origin_in_index_bool = False
-        origin_in_index_T_bool = False
-        self.origin = origin  
+        origin_in_index_T_bool = False 
         origin_match = [i for i in origin_names if i in self.data.columns]
         if len(origin_match)==1:
             self.origin = origin_match[0]
@@ -88,7 +93,7 @@ class triangle:
                 origin_in_index_bool = True
         return origin_in_col_bool, origin_in_index_bool
 
-    def __set_dev(self, dev):
+    def __set_dev(self):
         ##### Identify dev Profile ####
         dev_names = ['devpd', 'dev', 'development month', 'devyr', 'devyear']
         dev_in_col_bool = False
@@ -100,5 +105,24 @@ class triangle:
             dev_in_col_bool = True
         return dev_in_col_bool
     
+    def __set_values(self):
+        ##### Identify dev Profile ####
+        value_names = ['incurred claims'] 
+        values_match = [i for i in value_names if i in self.data.columns]
+        if len(values_match)==1:
+            self.values = values_match[0]
+        else:
+            self.values = 'values'
+        return 
 
+def plot(tri):
+    # Need lattice (cascade) option
+    plt.figure(figsize=(25,15))
+    for i in range(len(tri.data)):
+        plt.plot(tri.data.columns.astype(int),tri.data.iloc[i])
+        
+    
+    
+    
+    
     
