9cf669e535dacabc1fe2d950b8b1f0a3e68a1890
jbogaardt <jbogaardt@gmail.com>
2017-06-17 15:58:20 -0700

    fixed file structure
    fixed file structure

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 import sys
-sys.path.append(r'C:\Users\jboga\OneDrive\Documents\GitHub\chainladder-python')
+sys.path.append(r'C:\Users\jboga\OneDrive\Documents\GitHub\chainladder-python\chainladder')
 import chainladder as cl
 
 RAA = cl.triangle(cl.df_list[16])
