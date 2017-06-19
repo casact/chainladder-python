c580c6a91c998f601074ab99b2a13d0a442bccef
jbogaardt <jbogaardt@gmail.com>
2017-06-19 06:46:23 -0700

    updated conf.py for ReadThe Docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,28 @@ import sys
 #sys.path.append(r'C:\Users\jboga\OneDrive\Documents\GitHub\chainladder-python\chainladder')
 sys.path.append(os.path.abspath('..\chainladder'))
 
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
