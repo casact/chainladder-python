6e1d817cadfe377afaaf2a0de6977df180fb3266
jbogaardt <jbogaardt@gmail.com>
2017-06-19 09:43:32 -0700

    update __init__

--- chainladder/__init__.py
+++ chainladder/__init__.py
@@ -7,14 +7,14 @@ Created on Sat Jun 17 07:08:08 2017
 
 from pandas import DataFrame, Series, concat, pivot_table, read_pickle
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import os
 
 
 print('Welcome to chainladder')
 __all__ = ['Triangles', 'ChainLadder', 'MackChainLadderFunctions']
 
-sns.set_style("whitegrid")
+#sns.set_style("whitegrid")
 
 #from chainladder.TestCases import df_list
 from chainladder.classes import Triangle 
