e079e4fd1de2d1fb5fddc6fed1d35a7ebef7b633
jbogaardt <jbogaardt@gmail.com>
2017-06-25 14:54:20 -0700

    updated conf file

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ class Mock(object):
         else:
             return Mock()
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas', 'matplotlib.pyplot', 'scipy.interpolate', 'seaborn']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas', 'matplotlib.pyplot', 'scipy.interpolate', 'seaborn','statsmodels']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
