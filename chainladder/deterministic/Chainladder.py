34a271398615ac7f903e66096c882cd37e73286d
John Bogaardt <jbogaardt@gmail.com>
2018-06-30 07:56:10 -0600

    bug fix

--- chainladder/deterministic/Chainladder.py
+++ chainladder/deterministic/Chainladder.py
@@ -331,18 +331,18 @@ class Chainladder():
         if inplace == True:
             if triangle is None:
                 triangle = self.triangle
-            latest = triangle.get_latest_diagonal()
-            latest = np.array(latest[exposure>0])
-            CDF = np.array(self.CDF)[latest[:,0].astype(int)-1]
+            latest = np.array(triangle.get_latest_diagonal().iloc[:,-1])
+            latest = latest[exposure>0]
+            CDF = np.array(self.CDF)[::-1]
             reported_exposure = np.array(exposure[exposure>0])/ CDF
             trend_array = np.array([(1+trend)**(sum(exposure>0) - (i+1)) for i in range(sum(exposure>0))])
             decay_matrix = np.array([[decay**abs(i-j) for i in range(sum(exposure>0) )] for j in range(sum(exposure>0))])
             weighted_exposure = reported_exposure * decay_matrix
-            trended_ultimate = np.repeat(np.array([(latest[:,1] * trend_array) /(reported_exposure)]),sum(exposure>0),axis=0)
+            trended_ultimate = np.repeat(np.array([(latest * trend_array) /(reported_exposure)]),sum(exposure>0),axis=0)
             apriori = np.sum(weighted_exposure*trended_ultimate,axis=1)/np.sum(weighted_exposure,axis=1)
             detrended_ultimate = apriori/trend_array
             IBNR = detrended_ultimate * (1-1/CDF) * np.array(exposure[exposure>0])
-            self.ultimates = Series(latest[:,1] + IBNR, index=exposure[exposure>0].index)
+            self.ultimates = Series(latest + IBNR, index=exposure[exposure>0].index)
             self.method = 'cape_cod'
             self.exposure = exposure
             self.trend=trend
