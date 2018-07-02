21e4112dd278b0cbed751f41cd015789dc09d7a1
John Bogaardt <jbogaardt@gmail.com>
2018-07-02 06:10:07 -0600

    added holoviews dependency

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "seaborn>=0.7.1",
         "scipy>=0.19.0",
         "statsmodels>=0.8.0",
-        "bokeh>=0.11.0"
+        "bokeh>=0.11.0",
+        "holoviews>=1.10.0"
     ],
 )
