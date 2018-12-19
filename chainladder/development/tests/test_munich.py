2ee7d8d316346e287cc24e80bf1bcc332a7bb737
John Bogaardt <jbogaardt@gmail.com>
2018-12-19 06:38:25 -0700

    test

--- chainladder/development/tests/test_munich.py
+++ chainladder/development/tests/test_munich.py
@@ -22,3 +22,10 @@ def test_mcl_incurred():
     expected = np.array(pandas2ri.ri2py_dataframe(df))
     expected = np.reshape(expected, np.swapaxes(p, 0, 1).shape).T
     assert_allclose(expected, p, atol=1e-5)
+
+
+def test_rpy2():
+    df = r(f'MunichChainLadder(MCLpaid, MCLincurred)').rx('MCLPaid')
+    p = cl.MunichDevelopment(paid_to_incurred={'paid':'incurred'}).fit(cl.Development(sigma_interpolation='mack').fit_transform(cl.load_dataset('mcl'))).get_MCL_full_triangle()[0,0,0,:,:]
+    expected = np.array(pandas2ri.ri2py_dataframe(df))
+    assert expected == [1]
