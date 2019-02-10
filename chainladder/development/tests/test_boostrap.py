6b999fdf1f1179b81887e69c94d2469550856c1e
John Bogaardt <jbogaardt@gmail.com>
2019-02-09 18:11:44 -0700

    Update test_boostrap.py

--- chainladder/development/tests/test_boostrap.py
+++ chainladder/development/tests/test_boostrap.py
@@ -4,6 +4,6 @@ import numpy as np
 
 def test_bs_sample():
     tri = cl.load_dataset('raa')
-    a = cl.Development().fit(cl.BootstrapSample(n_sims=40000).fit_transform(tri).mean()).ldf_
+    a = cl.Development().fit(cl.BootstrapODPSample(n_sims=40000).fit_transform(tri).mean()).ldf_
     b = cl.Development().fit_transform(tri).ldf_
-    assert np.all(abs(((a-b)/b).triangle)<.001)
+    assert np.all(abs(((a-b)/b).triangle)<.005)
