d3fcb560e017b5532aba68f17b7b4e9973fab70f
John S Bogaardt <jbogaardt@gmail.com>
2020-01-19 17:51:12 -0700

    docs

--- examples/plot_bootstrap.py
+++ examples/plot_bootstrap.py
@@ -1,53 +1,33 @@
 """
-================================
-Stochastic Bornhuetter Ferguson
-================================
+======================
+ODP Bootstrap Example
+======================
 
-There are several aspects of the chainladder module that are demonstrated with
-this example.
-
-1. We see how to use the `BootstrapODPSample` and `BornhuetterFerguson` to come
-  up with a stochastic view of the Bornhuetter-Ferguson method.
-2. We see how we can use the `Triangle.values` property `numpy` to modify the
-  the data underlying the Triangle
-3. We use the `broadcast_axis` method of the triangle class (new in 0.4.7)
-
+This example demonstrates how you can can use the Overdispersed Poisson
+Bootstrap sampler and get various properties about parameter uncertainty.
 """
 import chainladder as cl
-import numpy as np
 
-# Simulation parameters
-random_state = 42
-n_sims = 1000
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style('whitegrid')
 
-# Get data
-loss = cl.load_dataset('genins')
-premium = loss.latest_diagonal*0+8e6
 
-# Simulate loss triangles
-sim = cl.BootstrapODPSample(random_state=random_state, n_sims=n_sims)
-sim.fit(loss, sample_weight=premium)
+#  Grab a Triangle
+tri = cl.load_dataset('genins')
+# Generate bootstrap samples
+sims = cl.BootstrapODPSample().fit_transform(tri)
+# Calculate LDF for each simulation
+sim_ldf = cl.Development().fit(sims).ldf_
+sim_ldf = sim_ldf[sim_ldf.origin==sim_ldf.origin.max()]
 
-# Repeat the premium triangle to align with simulated losses
-sim_p = premium.broadcast_axis('index', sim.resampled_triangles_.index)
-
-# Simulate aprioris using numpy
-apriori_mu = 0.65
-apriori_sigma = .10
-aprioris = np.random.normal(apriori_mu, apriori_sigma, n_sims)
-sim_p.values = (sim_p.values * aprioris.reshape(n_sims,-1)[..., np.newaxis, np.newaxis])
-
-# Fit Bornhuetter-Ferguson to stochastically generated data
-model = cl.BornhuetterFerguson().fit(sim.resampled_triangles_, sample_weight=sim_p)
-
-# Grab completed triangle replacing simulated known data with actual known data
-full_triangle = model.full_triangle_ - model.X_ + \
-                loss.broadcast_axis('index', sim.resampled_triangles_.index)
-
-# Limiting to the current year for plotting
-current_year = full_triangle[full_triangle.origin==full_triangle.origin.max()].to_frame().T
-
-# Plot the data
-current_year.reset_index(drop=True).plot(
-    color='orange', legend=False, alpha=0.1,
-    title='Current Accident Year BornFerg Distribution', grid=True);
+# Plot the Data
+fig, ((ax00, ax01), (ax10, ax11)) = plt.subplots(ncols=2, nrows=2, figsize=(10,10))
+tri.T.plot(ax=ax00).set(title='Raw Data', xlabel='Development', ylabel='Incurred')
+sims.mean().T.plot(ax=ax01).set(title='Mean Simulation', xlabel='Development', ylabel='Incurred')
+sim_ldf.T.plot(legend=False, color='lightgray', ax=ax10) \
+       .set(title='Simulated LDF', xlabel='Development', ylabel='LDF')
+cl.Development().fit(tri).ldf_.drop_duplicates().T \
+                .plot(legend=False, color='red', ax=ax10)
+sim_ldf.T.loc['12-24'].plot(kind='hist', bins=50, alpha=0.5, ax=ax11) \
+                      .set(title='Age 12-24 LDF Distribution', xlabel='LDF');
