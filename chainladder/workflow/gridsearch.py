73cc2afa3847b1f6e68b45948b889b1db4f2fe66
John S Bogaardt <jbogaardt@gmail.com>
2020-01-26 17:35:28 -0700

    Update gridsearch.py

--- chainladder/workflow/gridsearch.py
+++ chainladder/workflow/gridsearch.py
@@ -129,7 +129,14 @@ class Pipeline(PipelineSL, EstimatorIO):
         if sample_weight:
             fit_params = {} if not fit_params else fit_params
             fit_params[self.steps[-1][0] + '__sample_weight'] = sample_weight
-        super().fit(X, y, **fit_params)
+        return super().fit(X, y, **fit_params)
+
+    def predict(self, X, sample_weight=None):
+        if sample_weight:
+            fit_params = {} if not fit_params else fit_params
+            fit_params[self.steps[-1][0] + '__sample_weight'] = sample_weight
+        return super().predict(X, y, **fit_params)
+
 
     def to_json(self):
         return json.dumps(
