c189e1b61ec80980a66c931ac827941bcc2cc724
John S Bogaardt <jbogaardt@gmail.com>
2020-02-08 11:33:24 -0700

    Update test_incremental.py

--- chainladder/development/tests/test_incremental.py
+++ chainladder/development/tests/test_incremental.py
@@ -7,8 +7,8 @@ def test_schmidt():
     tri = cl.load_dataset('ia_sample')
     xp = cp.get_array_module(tri.values)
     ia = cl.IncrementalAdditive()
-    answer = ia.fit_transform(tri.iloc[0, 1],
-                              sample_weight=tri.iloc[0, 0].latest_diagonal)
+    answer = ia.fit_transform(tri.iloc[0, 0],
+                              sample_weight=tri.iloc[0, 1].latest_diagonal)
     answer = answer.incremental_.incr_to_cum().values[0, 0, :, -1]
     check = xp.array([3483., 4007.84795031, 4654.36196862, 5492.00685523,
                       6198.10197128, 7152.82539296])
