644a30c30ba3620978985463e3ec73f958ba0da5
John Bogaardt <jbogaardt@wcf.com>
2022-01-28 13:07:05 -0700

    malformed initialization

--- chainladder/__init__.py
+++ chainladder/__init__.py
@@ -43,4 +43,4 @@ from chainladder.tails import *  # noqa (API Import)
 from chainladder.methods import *  # noqa (API Import)
 from chainladder.workflow import *  # noqa (API Import)
 
-__version__ = "0.8.10"
+__version__ = "0.8.11"
