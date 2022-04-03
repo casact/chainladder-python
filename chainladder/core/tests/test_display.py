3d3fe012d3e0547b303523418225684125f62e4c
Kenneth S. Hsu <kennethshsu@gmail.com>
2022-04-02 22:25:24 -0700

    Added some test cases

--- chainladder/core/tests/test_display.py
+++ chainladder/core/tests/test_display.py
@@ -1,6 +1,20 @@
 import pytest
+import chainladder as cl
 
 
 def test_heatmap_render(raa):
     """ The heatmap method should render correctly given the sample."""
     return raa.heatmap()
+
+
+def test_to_frame(raa):
+    try:
+        cl.Chainladder().fit(raa).cdf_.to_frame()
+        cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=False)
+        cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=True)
+        cl.Chainladder().fit(raa).ultimate_.to_frame()
+        cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=False)
+        cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=True)
+        True == False
+    except:
+        assert False
