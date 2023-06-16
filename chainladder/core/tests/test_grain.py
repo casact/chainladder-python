aaba58eec973f69c67080df502dbfbd4d04c5fb2
Kenneth Hsu <kennethshsu@gmail.com>
2023-06-15 20:11:04 -0700

    Added test for devlopment age (quarterly)

--- chainladder/core/tests/test_grain.py
+++ chainladder/core/tests/test_grain.py
@@ -100,5 +100,11 @@ def test_annual_trailing(prism):
 
 
 def test_development_age():
-    raa_tri = cl.load_sample("raa")
-    assert (raa_tri.ddims == [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]).all()
+    assert (cl.load_sample("raa").ddims == [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]).all()
+
+def test_development_age2()
+    
+   assert (cl.load_sample("quarterly").ddims == [  3,   6,   9,  12,  15,  18,  21,  24,  27,  30,  33,  36,  39,
+        42,  45,  48,  51,  54,  57,  60,  63,  66,  69,  72,  75,  78,
+        81,  84,  87,  90,  93,  96,  99, 102, 105, 108, 111, 114, 117,
+       120, 123, 126, 129, 132, 135]).all()
