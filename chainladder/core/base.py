347d48c737619727cd27dc6e8c886269712d35c6
Kenneth Hsu <kennethshsu@gmail.com>
2024-05-10 22:24:36 -0700

    Began work on deprecating infer_datetime_format

--- chainladder/core/base.py
+++ chainladder/core/base.py
@@ -237,19 +237,25 @@ class TriangleBase(
                 return target.dt.to_timestamp(how={1: "e", 0: "s"}[period_end])
         else:
             datetime_arg = target_field.unique()
+            # print("datetime_arg", datetime_arg)
             format = [{"arg": datetime_arg, "format": format}] if format else []
             date_inference_list = format + [
                 {"arg": datetime_arg, "format": "%Y%m"},
                 {"arg": datetime_arg, "format": "%Y"},
-                {"arg": datetime_arg, "infer_datetime_format": True},
+                {"arg": datetime_arg, "format": "%Y-%m-%d"},
+                # {"arg": datetime_arg, "infer_datetime_format": True},
             ]
+            print("date_inference_list", date_inference_list)
             for item in date_inference_list:
+                print("trying:", item)
                 try:
                     arr = dict(zip(datetime_arg, pd.to_datetime(**item)))
                     break
                 except:
                     pass
+
             target = target_field.map(arr)
+            # print("target", target)
         return target
 
     @staticmethod
