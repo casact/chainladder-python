cfa416711db31561240ce043d75a89aad5d5ab77
Kenneth Hsu <kennethshsu@gmail.com>
2024-05-16 17:29:09 -0700

    Addressed "FutureWarning: 'Y-DEC' is deprecated and will be removed in a future version, please use 'YE-DEC' instead."

--- chainladder/core/triangle.py
+++ chainladder/core/triangle.py
@@ -690,7 +690,7 @@ class Triangle(TriangleBase):
         obj = self.dev_to_val()
 
         if ograin_new != ograin_old:
-            freq = {"Y": "Y", "S": "2Q"}.get(ograin_new, ograin_new)
+            freq = {"Y": "YE", "S": "2Q"}.get(ograin_new, ograin_new)
 
             if trailing or (obj.origin.freqstr[-3:] != "DEC" and ograin_old != "M"):
                 origin_period_end = self.origin[-1].strftime("%b").upper()
