02a088ff61f003cde785d55448a6654eec2f95f8
henrydingliu@gmail.com <henrydingliu@gmail.com>
2025-12-31 19:35:33 +0000

    fixing tests

--- chainladder/development/tests/test_glm.py
+++ chainladder/development/tests/test_glm.py
@@ -8,6 +8,6 @@ def test_basic_odp_cl(genins):
     
 def test_sample_weight(genins):
     assert abs(
-        (cl.Chainladder().fit(cl.TweedieGLM().fit_transform(genins,sample_weight=genins/genins)).ultimate_ -
-         cl.Chainladder().fit(cl.TweedieGLM().fit_transform(genins)).ultimate_) /
+        (cl.Chainladder().fit(genins).ultimate_ -
+         cl.Chainladder().fit(cl.TweedieGLM().fit_transform(genins,sample_weight=genins/genins)).ultimate_) /
         genins.latest_diagonal).max()< 1e-2
--- chainladder/development/tests/test_learning.py
+++ chainladder/development/tests/test_learning.py
@@ -3,7 +3,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from chainladder.utils.utility_functions import PatsyFormula
 
-def test_basic_odp_cl(genins):
+def test_incremental(genins):
+    response = [genins.columns[0]]
     model = cl.DevelopmentML(Pipeline(steps=[
         ('design_matrix', PatsyFormula('C(development)')),
         ('model', LinearRegression(fit_intercept=False))]),
