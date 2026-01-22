6f552ec04639a67fdd2cdcffb4c4e8a327078694
danielfong-act <danielfong247@gmial.com>
2026-01-21 19:56:00 -0800

    added a simple test to make sure grain doesn't get mussed up in DevelopmentML

--- chainladder/development/tests/test_learning.py
+++ chainladder/development/tests/test_learning.py
@@ -2,6 +2,7 @@ import chainladder as cl
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from chainladder.utils.utility_functions import PatsyFormula
+import pandas as pd
 
 def test_incremental(genins):
     response = [genins.columns[0]]
@@ -17,3 +18,19 @@ def test_misc(genins):
         ('model', LinearRegression(fit_intercept=False))]),
                 weighted_step = ['model'], fit_incrementals=False).fit(genins, sample_weight=genins/genins)
     assert abs(model.triangle_ml_.loc[:,:,'2010',:] - genins.mean()).max() < 1e2
+    
+def test_grain():
+    grains={'Y':2,'2Q':4,'Q':8,'M':24}
+    for period in grains.keys():
+        tframe = pd.DataFrame()
+        devdates=pd.date_range(start='2000-01-01',end='2002-01-01',freq=period+'S',inclusive='left')
+        for ind, sdate in enumerate(devdates):
+            tframe=pd.concat([tframe,pd.DataFrame({'origin':[sdate]*(len(devdates)-ind),
+            'development':pd.date_range(start=sdate,end='2002-01-01',freq=period+'S',inclusive='left'),
+            'loss':[100]*(len(devdates)-ind)})])
+        tri= cl.Triangle(tframe,origin='origin',development='development',columns='loss',cumulative=False)
+        model = cl.DevelopmentML(Pipeline(steps=[
+        ('design_matrix', PatsyFormula('C(development)')),
+        ('model', LinearRegression(fit_intercept=False))]),
+                weighted_step = ['model'], fit_incrementals=False).fit(tri)
+        assert grains[period] == len(model.triangle_ml_.development) == len(model.triangle_ml_.origin)
