9f5fdac6954df9b52033dc1c9255409c8332193e
Gene Dan <genedan@gmail.com>
2026-04-23 15:28:12 -0500

    TEST: Fix test paths to repository and docs helper functions.

--- conftest.py
+++ conftest.py
@@ -1,10 +1,5 @@
-import pathlib
 import pytest
-import sys
 import chainladder as cl
-
-# Add path to documentation helper functions.
-sys.path.insert(0, str(pathlib.Path(__file__).parent / 'docs' / '_ext'))
 
 
 def pytest_generate_tests(metafunc):
