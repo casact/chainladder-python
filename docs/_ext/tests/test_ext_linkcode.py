298366bdd4dfec11ea06e720d1bd985e36a4028c
Gene Dan <genedan@gmail.com>
2026-04-23 17:40:59 -0500

    Add links to object source code in API Reference section (#710)
    * DOCS: Add linkcode extension and associated linkcode_resolve function. Used to link documentation to source code.
    
    * TEST: Add unit test for linkcode_resolve.
    
    * TEST: Fix test paths to repository and docs helper functions.
    
    * FIX: Fix null check issue.
    
    * FIX: Fix relative pathing.
    
    * FIX: Fix relative pathing.
    
    * FIX: Fix relative pathing.
    
    * FIX: Fix relative pathing.
    
    * FIX: Fix relative pathing.

---
+++ docs/_ext/tests/test_ext_linkcode.py
@@ -0,0 +1,27 @@
+from docs._ext.ext_linkcode import linkcode_resolve # noqa
+
+def test_linkcode_resolve_url_lines() -> None:
+    """
+    Tests the linkcode_resolve function. Ideally we want:
+
+     - A URL that points to GitHub
+     - Starting and ending line numbers in the URL.
+    """
+    url: str = linkcode_resolve(
+        domain='py',
+        info={
+            'module': 'chainladder',
+            'fullname': 'Benktander'
+        }
+    )
+
+    assert url is not None
+    assert url.startswith('https://github.com/casact/chainladder-python/blob/master/')
+
+    # Extract the line numbers.
+    lines: str = url.split('#')[1]
+    start, end = [int(x[1:]) for x in lines.split('-')]
+
+    # Check the line numbers.
+    assert start > 0
+    assert end > start
