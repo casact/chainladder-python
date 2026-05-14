72b270c3499992ce5fd0c0b06889680f7cfe467a
henrydingliu@gmail.com <henrydingliu@gmail.com>
2026-05-14 22:13:15 +0000

    cleaning up and adding some tests

--- chainladder/core/tests/test_triangle.py
+++ chainladder/core/tests/test_triangle.py
@@ -1026,12 +1026,19 @@ def test_validate_assumption(raa: Triangle) -> None:
         )
 
 def test_xs(clrd):
-    assert clrd.xs('Adriatic Ins Co') == clrd.loc['Adriatic Ins Co']
     # when slicing with .loc on the first term in the index, Triangle will drop the term 
-    assert clrd.xs('Adriatic Ins Co',drop_level = True).index.equals(clrd.loc['Adriatic Ins Co'].index)
-    assert clrd.xs(('Agway Ins Co','comauto')) == clrd.loc['Agway Ins Co','comauto']
-    assert clrd.xs(('Agway Ins Co','comauto')).index.equals(clrd.loc['Agway Ins Co','comauto'].index)
+    assert clrd.xs('Adriatic Ins Co') == clrd.loc['Adriatic Ins Co']
+    assert clrd.xs('Adriatic Ins Co').index.equals(clrd.loc['Adriatic Ins Co'].index)
+    # when slicing with .loc on the all term in the index, Triangle will not drop any term 
+    assert clrd.xs(('Agway Ins Co','comauto'), drop_level=False) == clrd.loc['Agway Ins Co','comauto']
+    assert clrd.xs(('Agway Ins Co','comauto'), drop_level=False).index.equals(clrd.loc['Agway Ins Co','comauto'].index)
     # when all index terms are included in xs and drop_level is True, the default 'Total' index value is provided
     assert clrd.xs(('Agway Ins Co','comauto'), drop_level=True).index.equals(cl.load_sample('genins').index)
-    assert clrd.xs('comauto',level=1) == clrd.loc[clrd['LOB'] == 'comauto']
-    assert clrd.xs('comauto',level=1).index.equals(clrd.loc[clrd['LOB'] == 'comauto'].index)
+    # when slicing with .loc on the second or subsequent terms in the index, Triangle will not drop the term 
+    assert clrd.xs('comauto',level=1, drop_level=False) == clrd.loc[clrd['LOB'] == 'comauto']
+    assert clrd.xs('comauto',level=1, drop_level=False).index.equals(clrd.loc[clrd['LOB'] == 'comauto'].index)
+    # level works with either integer index or name of the index column
+    assert clrd.xs('comauto',level=1) == clrd.xs('comauto',level='LOB')
+    assert clrd.xs('comauto',level=1).index.equals(clrd.xs('comauto',level='LOB').index)
+
+
