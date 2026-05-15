7b5f7bc5f335bf571597cd28eb20f18cf1d409ac
henrydingliu@gmail.com <henrydingliu@gmail.com>
2026-05-15 15:25:16 +0000

    improving test

--- chainladder/development/tests/test_constant.py
+++ chainladder/development/tests/test_constant.py
@@ -40,7 +40,7 @@ def test_constant_callable_axis0(clrd, atol):
     assert abs(model.fit_transform(agway).cdf_.loc['comauto'].iloc[..., 0].sum() - 3.832) < atol
 
 def test_constant_callable_axis1(clrd, atol):
-    agway = cl.load_sample('clrd').loc['Agway Ins Co', 'comauto']
+    agway = clrd.loc['Agway Ins Co', 'comauto']
     cdfs = {
         'IncurLoss': [3.832, 1.874, 1.386, 1.181, 1.085, 1.043, 1.022, 1.013, 1.007, 1],
         'CumPaidLoss': [24.168, 4.127, 2.103, 1.528, 1.275, 1.161, 1.088, 1.047, 1.018, 1],
@@ -48,12 +48,11 @@ def test_constant_callable_axis1(clrd, atol):
         'EarnedPremDIR': [2.559, 1.417, 1.181, 1.084, 1.04, 1.019, 1.009, 1.004, 1.001, 1],
         'EarnedPremCeded': [13.703, 5.613, 2.92, 1.765, 1.385, 1.177, 1.072, 1.034, 1.008, 1],
         'EarnedPremNet': [4.106, 1.865, 1.418, 1.234, 1.141, 1.09, 1.056, 1.03, 1.01, 1]}
+    patterns = pd.DataFrame(cdfs, index=range(12, 132, 12)).T
     def paid_cdfs(x):
         """ A function that returns different CDFs depending on a specified column """
-        patterns = pd.DataFrame(cdfs, index=range(12, 132, 12)).T
         return patterns.loc[x.loc['columns']].to_dict()
     with pytest.raises(ValueError):
         xerror = cl.DevelopmentConstant(patterns=paid_cdfs, callable_axis=2, style='cdf').fit(agway)
-    lhs = cl.DevelopmentConstant(patterns=paid_cdfs, callable_axis=1, style='cdf').fit(agway).cdf_.sum().sum()
-    rhs = sum([sum(l[:-1]) for l in cdfs.values()])
-    assert abs(lhs - rhs) < atol
+    lhs = cl.DevelopmentConstant(patterns=paid_cdfs, callable_axis=1, style='cdf').fit(agway).cdf_
+    assert np.all(abs(lhs.values[0,:,0,:]-patterns.values[:,:-1]) < atol)
