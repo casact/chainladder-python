6ae3f72405565a82c1dad2a9383e15856595975c
Kenneth Hsu <kennethshsu@gmail.com>
2026-05-15 14:16:00 -0700

    Flow control geometric vs not

--- chainladder/development/development.py
+++ chainladder/development/development.py
@@ -186,38 +186,71 @@ class Development(DevelopmentBase):
         print("self.w_\n", self.w_)
 
         # fitting the regression parameters
-        params = WeightedRegression(axis=2, thru_orig=True, xp=xp).fit(x, y, w)
-
-        if self.n_periods != 1:
-            params = params.sigma_fill(self.sigma_interpolation)
-        else:
-            warnings.warn(
-                "Setting n_periods=1 does not allow enough degrees "
-                "of freedom to support calculation of all regression "
-                "statistics. Only LDFs have been calculated."
-            )
-
-        params.std_err_ = xp.nan_to_num(params.std_err_) + xp.nan_to_num(
-            (1 - xp.nan_to_num(params.std_err_ * 0 + 1))
-            * params.sigma_
-            / xp.swapaxes(xp.sqrt(x ** (2 - exponent))[..., 0:1, :], -1, -2)
-        )
-
-        params = xp.concatenate((params.slope_, params.sigma_, params.std_err_), 3)
-        params = xp.swapaxes(params, 2, 3)
-
-        self.ldf_ = self._param_property(obj, params, 0)
-        self.sigma_ = self._param_property(obj, params, 1)
-        self.std_err_ = self._param_property(obj, params, 2)
-        print("self.ldf_\n", self.ldf_)
-
-        resid = -obj.iloc[..., :-1] * self.ldf_.values + obj.iloc[..., 1:].values
-        std = xp.sqrt((1 / num_to_nan(w)) * (self.sigma_**2).values)
-        resid = resid / num_to_nan(std)
-        self.std_residuals_ = resid[resid.valuation < obj.valuation_date].fillzero()
-
         if "geometric" in average_[0, 0, 0]:
             print("in geometric - needs additional processing")
+            print("link_ratio\n", link_ratio)
+            print("self.w_\n", self.w_)
+
+            # params = WeightedRegression(axis=2, thru_orig=True, xp=xp).fit(x, y, w)
+
+            # from chainladder.utils.utility_functions import num_to_nan
+
+            # w, x, y, axis = self.w.copy(), self.x.copy(), self.y.copy(), self.axis
+            # xp = self.xp
+            # if xp != sp:
+            #     x[w == 0] = xp.nan
+            #     y[w == 0] = xp.nan
+            # else:
+            #     w2 = w.copy()
+            #     w2 = sp.COO(
+            #         data=w2.data, coords=w2.coords, fill_value=sp.nan, shape=w2.shape
+            #     )
+            #     x, y = x * w2, y * w2
+
+            # with warnings.catch_warnings():
+            #     warnings.simplefilter("ignore", category=RuntimeWarning)
+            #     slope = num_to_nan(
+            #         xp.nansum(w * x * y, axis)
+            #         - xp.nansum(x * w, axis) * xp.nanmean(y, axis)
+            #     ) / num_to_nan(
+            #         xp.nansum(w * x * x, axis)
+            #         - xp.nanmean(x, axis) * xp.nansum(w * x, axis)
+            #     )
+            #     intercept = xp.nanmean(y, axis) - slope * xp.nanmean(x, axis)
+
+            # self.slope_ = slope[..., None]
+            # self.intercept_ = intercept[..., None]
+
+        else:
+            params = WeightedRegression(axis=2, thru_orig=True, xp=xp).fit(x, y, w)
+
+            if self.n_periods != 1:
+                params = params.sigma_fill(self.sigma_interpolation)
+            else:
+                warnings.warn(
+                    "Setting n_periods=1 does not allow enough degrees "
+                    "of freedom to support calculation of all regression "
+                    "statistics. Only LDFs have been calculated."
+                )
+
+            params.std_err_ = xp.nan_to_num(params.std_err_) + xp.nan_to_num(
+                (1 - xp.nan_to_num(params.std_err_ * 0 + 1))
+                * params.sigma_
+                / xp.swapaxes(xp.sqrt(x ** (2 - exponent))[..., 0:1, :], -1, -2)
+            )
+
+            params = xp.concatenate((params.slope_, params.sigma_, params.std_err_), 3)
+            params = xp.swapaxes(params, 2, 3)
+
+            self.ldf_ = self._param_property(obj, params, 0)
+            self.sigma_ = self._param_property(obj, params, 1)
+            self.std_err_ = self._param_property(obj, params, 2)
+            print("self.ldf_\n", self.ldf_)
+
+            resid = -obj.iloc[..., :-1] * self.ldf_.values + obj.iloc[..., 1:].values
+            std = xp.sqrt((1 / num_to_nan(w)) * (self.sigma_**2).values)
+            resid = resid / num_to_nan(std)
+            self.std_residuals_ = resid[resid.valuation < obj.valuation_date].fillzero()
 
         return self
 
