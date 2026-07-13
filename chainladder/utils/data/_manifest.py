62847d645528b45eae4d3f697865009bcfcdf284
Kenneth Hsu <kennethshsu@gmail.com>
2026-07-13 16:19:45 -0700

    Added premium to friedland_us_auto_chg_prod_mix

--- chainladder/utils/data/_manifest.py
+++ chainladder/utils/data/_manifest.py
@@ -186,7 +186,7 @@ SAMPLES: dict = {
         "origin": "Accident Year",
         "development": "Calendar Year",
         "index": None,
-        "columns": ["Paid Claims", "Reported Claims"],
+        "columns": ["Paid Claims", "Reported Claims", "Earned Premium"],
         "cumulative": True,
     },
     "friedland_us_auto_incr_claim": {
@@ -200,7 +200,7 @@ SAMPLES: dict = {
         "origin": "Accident Year",
         "development": "Calendar Year",
         "index": None,
-        "columns": ["Paid Claims", "Reported Claims"],
+        "columns": ["Paid Claims", "Reported Claims", "Earned Premium"],
         "cumulative": True,
     },
     "friedland_us_industry_auto": {
