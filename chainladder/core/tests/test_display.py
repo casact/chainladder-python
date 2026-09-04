a3b258e7101334855030daba14b7f22728389a0e
Gene Dan <genedan@gmail.com>
2026-09-03 22:04:57 -0500

    [FIX] Apply Ruff fixes.

--- chainladder/core/tests/test_display.py
+++ chainladder/core/tests/test_display.py
@@ -552,24 +552,16 @@ def test_display_import_fallback_when_ipython_missing() -> None:
 
 def test_heatmap_render(raa):
     """The heatmap method should render correctly given the sample."""
-    try:
-        raa.heatmap()
-
-    except:
-        assert False
+    raa.heatmap()
 
 
 def test_to_frame(raa):
-    try:
-        cl.Chainladder().fit(raa).cdf_.to_frame()
-        cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=False)
-        cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=True)
-        cl.Chainladder().fit(raa).ultimate_.to_frame()
-        cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=False)
-        cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=True)
-
-    except:
-        assert False
+    cl.Chainladder().fit(raa).cdf_.to_frame()
+    cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=False)
+    cl.Chainladder().fit(raa).cdf_.to_frame(origin_as_datetime=True)
+    cl.Chainladder().fit(raa).ultimate_.to_frame()
+    cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=False)
+    cl.Chainladder().fit(raa).ultimate_.to_frame(origin_as_datetime=True)
 
 
 def test_labels(xyz):
