4e1c30430c6c32a8136ca7e9419fe2f7bb0036f5
henrydingliu <106109320+henrydingliu@users.noreply.github.com>
2026-09-10 23:18:35 -0700

    Update test_wtd_reg.py

--- chainladder/utils/tests/test_wtd_reg.py
+++ chainladder/utils/tests/test_wtd_reg.py
@@ -10,28 +10,28 @@ class TestOLS:
 
     def test_missing_data(self) -> None:
         """Check that having nan in X and/or y still results in the right OLS coefficients."""
-        data = [{
-            "module": np,
-            "X": [
-                np.array([[[[1.0], [2.0], [3.0], [4.0], [5.0]]]]),
-                np.array([[[[1.0], [np.nan], [3.0], [4.0], [5.0]]]]),
-            ],
-            "y": [
-                np.array([[[[1.0], [2.0], [3.0], [4.0], [5.0]]]]),
-                np.array([[[[1.0], [2.0], [np.nan], [4.0], [5.0]]]]),
-            ],
-            "w": np.array([[[[1.0], [1.0], [1.0], [1.0], [1.0]]]]),
-            "slope": np.array([[[[1.0]]]]),
-        }]
-        data.append(
+        data = [
             {
+                "module": np,
+                "X": [
+                    np.array([[[[1.0], [2.0], [3.0], [4.0], [5.0]]]]),
+                    np.array([[[[1.0], [np.nan], [3.0], [4.0], [5.0]]]]),
+                ],
+                "y": [
+                    np.array([[[[1.0], [2.0], [3.0], [4.0], [5.0]]]]),
+                    np.array([[[[1.0], [2.0], [np.nan], [4.0], [5.0]]]]),
+                ],
+                "w": np.array([[[[1.0], [1.0], [1.0], [1.0], [1.0]]]]),
+                "slope": np.array([[[[1.0]]]]),
+            }
+        ]
+        data.append({
                 "module": sp,
                 "X": [sp.COO.from_numpy(i, fill_value=np.nan) for i in data[0]["X"]],
                 "y": [sp.COO.from_numpy(i, fill_value=np.nan) for i in data[0]["y"]],
                 "w": sp.COO.from_numpy(data[0]["w"]),
                 "slope": sp.COO.from_numpy(data[0]["slope"]),
-            }
-        )
+        })
         for i in data:
             for x in i["X"]:
                 for y in i["y"]:
