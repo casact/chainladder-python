3a7ae557517948d5fdbe96c0f53a22944157d692
henrydingliu <106109320+henrydingliu@users.noreply.github.com>
2026-09-18 17:10:20 -0700

    Update test_triangle.py

--- chainladder/core/tests/test_triangle.py
+++ chainladder/core/tests/test_triangle.py
@@ -3311,7 +3311,9 @@ def test_fill(clrd: Triangle) -> None:
     ``Fill`` method works as intended
     """
     fill_tri = clrd.iloc[2:4, 4:6].fill(100)
-    assert np.nansum(fill_tri.values) == 100 * 11 * 10 / 2 * 2 * 2
+    # (10 + 1) * 10 / 2 is the number of valid values in one single triangle
+    # multiplied by 2 index values and 2 column values
+    assert np.nansum(fill_tri.values) == 100 * (10 + 1) * 10 / 2 * 2 * 2
     assert np.nanmax(fill_tri.values) == 100
     assert np.nanmin(fill_tri.values) == 100
 
