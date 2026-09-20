b73c5a16b6ce7f572907aab97da44174f77ce263
henrydingliu@gmail.com <henrydingliu@gmail.com>
2026-09-20 07:28:47 +0000

    better sparse handling

--- chainladder/tails/base.py
+++ chainladder/tails/base.py
@@ -161,7 +161,14 @@ class TailBase(DevelopmentBase):
             self.ldf_.values[..., -self._ave_period[0] - 1 :], -1, keepdims=True
         )
         reg = WeightedRegression(axis=3, xp=xp).fit(None, xp.log(y - 1), None)
-        tail = xp.maximum(tail, [[[[1.001]]]])
+        if xp == "sparse":
+            tail_data = tail.data
+            valid_tail = tail_data[tail_data > 1]
+            smallest_valid_tail = np.nanmin(valid_tail)
+        elif:
+            valid_tail = tail[tail > 1]
+            smallest_valid_tail = xp.nanmin(valid_tail)
+        tail = xp.maximum(tail, [[[[smallest_valid_tail]]]])
         time_pd = (xp.log(tail - 1) - reg.intercept_) / reg.slope_
         return time_pd
 
