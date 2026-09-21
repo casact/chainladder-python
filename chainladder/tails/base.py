7378ab56015be286f690d802bd43252ef14d3902
henrydingliu@gmail.com <henrydingliu@gmail.com>
2026-09-21 01:48:28 +0000

    backtracking tail change

--- chainladder/tails/base.py
+++ chainladder/tails/base.py
@@ -161,14 +161,7 @@ class TailBase(DevelopmentBase):
             self.ldf_.values[..., -self._ave_period[0] - 1 :], -1, keepdims=True
         )
         reg = WeightedRegression(axis=3, xp=xp).fit(None, xp.log(y - 1), None)
-        # theoretically, this slicing doesn't work on sparse. however, no sparse
-        # tail ever reaches here because ldf_ is always numpy
-        valid_tail = tail[tail > 1]
-        if valid_tail.size > 0:
-            smallest_valid_tail = xp.nanmin(valid_tail)
-        else:
-            smallest_valid_tail = 1.001
-        tail = xp.maximum(tail, [[[[smallest_valid_tail]]]])
+        tail = tail if tail.max() > 1 else 1.001
         time_pd = (xp.log(tail - 1) - reg.intercept_) / reg.slope_
         return time_pd
 
